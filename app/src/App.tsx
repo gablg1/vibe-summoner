a9d2b161cca5cfd1304cbb69c7e69da1cf4dc08b
Gabriel Guimaraes <gabriel@brex.com>
2023-02-20 17:21:50 -0300

    Typescript

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,22 +7,23 @@ import Box from '@mui/material/Box';
 import Kick01 from './sounds/kicks/Kick_01.wav';
 import Snare01 from './sounds/snares/Snare_01.wav';
 
-let [kick_sample, snare_sample] = [null, null];
+let kick_sample: null | AudioBuffer = null;
+let snare_sample: null | AudioBuffer = null;
 
 const audioCtx = new AudioContext();
 
-async function getFile(audioContext, filepath) {
+async function getFile(audioContext: AudioContext, filepath: string) {
   const response = await fetch(filepath);
   const arrayBuffer = await response.arrayBuffer();
   const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
   return audioBuffer;
 }
 
-async function setupSample(audioContext, filePath) {
+async function setupSample(audioContext: AudioContext, filePath: string) {
   return await getFile(audioContext, filePath);
 }
 
-function playSample(audioContext, audioBuffer, time) {
+function playSample(audioContext: AudioContext, audioBuffer: AudioBuffer, time: number) {
   const sampleSource = new AudioBufferSourceNode(audioContext, {
     buffer: audioBuffer,
     playbackRate: 1,
@@ -33,11 +34,13 @@ function playSample(audioContext, audioBuffer, time) {
 }
 
 function playKick() {
-  playSample(audioCtx, kick_sample, 0);
+  // FIXME: rm "as"
+  playSample(audioCtx, kick_sample as AudioBuffer, 0);
 }
 
 function playSnare() {
-  playSample(audioCtx, snare_sample, 0);
+  // FIXME: rm "as"
+  playSample(audioCtx, snare_sample as AudioBuffer, 0);
 }
 
 async function loadSamples() {
